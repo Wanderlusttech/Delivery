d435505262e660567b37e6eb11c074dbdd13a9b4
Wanderlusttech <92966325+Wanderlusttech@users.noreply.github.com>
2022-11-23 18:51:12 -0800

    log in page

---
+++ js/sww.js
@@ -0,0 +1,25 @@
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+loginButton.addEventListener("click", (e) => {
+    e.preventDefault();
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === "nat" && password === "1234") {
+        
+        window.location.href = './Alexa.html';
+    } 
+
+    if (username === "ivy" && password === "1234") {
+        
+        window.location.href = './Ivy.html';
+    } 
+
+   
+    
+    else {
+        loginErrorMsg.style.opacity = 1;
+    }
+})
